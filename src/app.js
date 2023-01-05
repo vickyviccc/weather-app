d3121e748bd1c2248eeada279000cf99c4e3b74c
Vicky S <112182245+vickyviccc@users.noreply.github.com>
2023-01-05 20:45:16 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
-let iconElement = document.querySelector("icon");
+let iconElement = document.querySelector("#icon");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name; 
 descriptionElement.innerHTML = response.data.weather[0].description;
@@ -32,12 +32,25 @@ humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);  
 dateElement.innerHTML = formatDate (response.data.dt *1000);
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+
+
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-icon.Element.setAttribute("alt", response.data.weather[0].description);
 
-
+function search(city) {
 let apiKey = "34b48ef5fb51126e6839dd0ddea238ea";
-let city = "New York"; 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature); 
+}
+function handleSubmit(event) {
+    event.preventDefault(); 
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+    console.log(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
